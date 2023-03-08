970755b12dca72887d981fe86f58c4ca343f7038
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-03-08 20:53:09 +0100

    ajout 2 activitys vierges

---
+++ app/src/main/java/fr/epf/mm/gestionclient2023min2/DetailsClientActivity.kt
@@ -0,0 +1,11 @@
+package fr.epf.mm.gestionclient2023min2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_client)
+    }
+}
---
+++ app/src/main/java/fr/epf/mm/gestionclient2023min2/ListClientActivity.kt
@@ -0,0 +1,11 @@
+package fr.epf.mm.gestionclient2023min2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_client)
+    }
+}
