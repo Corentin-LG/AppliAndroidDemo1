9f80d53d3bb46fb79d4d3f9b8633f8f43c75a09d
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-03-14 19:02:50 +0100

    tests affichage un patient

--- app/src/main/java/fr/epf/mm/gestionclient2023min2/HomeActivity.kt
+++ app/src/main/java/fr/epf/mm/gestionclient2023min2/HomeActivity.kt
@@ -3,6 +3,8 @@ package fr.epf.mm.gestionclient2023min2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
@@ -11,9 +13,19 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val addButton = findViewById<Button>(R.id.home_add_button)
+        val listButton = findViewById<Button>(R.id.home_list_button)
         addButton.setOnClickListener {
             val intent = Intent(this, AddClientActivity::class.java)
             startActivity(intent)
         }
+        listButton.click {
+            val intent = Intent(this, ListClientActivity::class.java)
+            startActivity(intent)
+        }
     }
+}
+
+fun View.click (action : (View) -> Unit){
+    Log.d("CLICK", "click")
+    this.setOnClickListener(action)
 }
--- app/src/main/java/fr/epf/mm/gestionclient2023min2/ListClientActivity.kt
+++ app/src/main/java/fr/epf/mm/gestionclient2023min2/ListClientActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import fr.epf.mm.gestionclient2023min2.model.Client
 
 class ListClientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,6 @@ class ListClientActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.list_client_recycleview)
 
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        recyclerView.adapter = ClientAdapter()
+        recyclerView.adapter = ClientAdapter(Client.all())
     }
 }
